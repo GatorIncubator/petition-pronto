661dd858227efc4741b1395a340aac74d03a44ac
Austin Bristol <bristola@aldenv106.allegheny.edu>
2019-04-09 20:35:44 -0400

    Added error page

--- src/app.py
+++ src/app.py
@@ -14,5 +14,10 @@ def home_get():
     return render_template('home.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run()
