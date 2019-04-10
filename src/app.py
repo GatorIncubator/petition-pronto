d803957ce9f26f669667b3b9ec3596a7c9905cf2
Austin Bristol <bristola@aldenv106.allegheny.edu>
2019-04-09 20:48:53 -0400

    Somework on login

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask, Response, redirect, url_for, request, abort
+from flask import render_template, Flask, Response, redirect, url_for, request, abort, session
 
 
 app = Flask(__name__)
@@ -11,7 +11,36 @@ def home_redirect():
 
 @app.route("/home", methods=["GET"])
 def home_get():
-    return render_template('home.html')
+    if not session.get('logged_in'):
+        return render_template('home.html')
+    else:
+        return redirect("/petitions")
+
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return redirect("/petitions")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form['email']
+    password = request.form['password']
+    if email == "email" and password == "password":
+        return redirect("/petitions")
+    else:
+        redirect("/login")
+
+
+@app.route("/petitions", methods=["GET"])
+def petitions_get():
+    if session.get('logged_in'):
+        return render_template('petitions.html')
+    else:
+        return redirect("/home")
 
 
 @app.errorhandler(404)
