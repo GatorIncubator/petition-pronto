e95e773f94c20319d233afbccc9f7a07a18b3c88
clussier <lussierc@allegheny.edu>
2019-04-16 16:59:49 -0400

    Add support for SQLite3 logins.

--- src/login_handler.py
+++ src/login_handler.py
@@ -10,13 +10,26 @@ tables_dict = {"User_Table": 5,
 
 def validate_user(email, password):
     """Validates the user identity using information from database."""
+    user_email = email
+    user_password = password
     conn = sqlite3.connect("petitiondb.sqlite3") # connect to the database
+
     email_query = "SELECT email FROM User_Table WHERE email = \"{A}\"".format(A = email)
-    email_result = conn.execute(email_query)
+    email_query_obj = conn.execute(email_query)
+    email_tuple = email_query_obj.fetchone()
+    try:
+        email_result = email_tuple[0]
+    except:
+        email_result = ""
+
+
     password_query = "SELECT password FROM User_Table WHERE email = \"{A}\"".format(A = email)
-    password_result = conn.execute(password_query)
+    password_query_obj = conn.execute(password_query)
+    password_tuple = password_query_obj.fetchone()
+    password_result = password_tuple[0]
 
     #if email == email_result and password == password_result:
+    print("***If", email, "==", email_result, "and", password, "==", password_result)
     if email == email_result and password == password_result:
         return True
     else:
