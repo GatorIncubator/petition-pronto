d104568c3f9ea018f76a03c270f17dc0b02dba31
clussier <lussierc@allegheny.edu>
2019-04-16 17:15:39 -0400

    Add comments to login handler.

--- src/login_handler.py
+++ src/login_handler.py
@@ -10,28 +10,27 @@ tables_dict = {"User_Table": 5,
 
 def validate_user(email, password):
     """Validates the user identity using information from database."""
-    conn = sqlite3.connect("petitiondb.sqlite3") # connect to the database
+    conn = sqlite3.connect("petitiondb.sqlite3")  # connect to the database
 
     email_query = "SELECT email FROM User_Table WHERE email = \"{A}\"".format(A = email)
-    email_query_obj = conn.execute(email_query) # execute query that checks for valid email
-    email_tuple = email_query_obj.fetchone() # store results of query - in a tuple
+    email_query_obj = conn.execute(email_query)  # execute query that checks for valid email
+    email_tuple = email_query_obj.fetchone()  # store results of query - in a tuple
     try:
-        email_result = email_tuple[0]
+        email_result = email_tuple[0]  # if query returns a result, store it as a string
     except:
-        email_result = "" # if query returns no results, store it as an empty string
+        email_result = ""  # if query returns no results/an error, set value as empty string
 
     password_query = "SELECT password FROM User_Table WHERE email = \"{A}\"".format(A = email)
-    password_query_obj = conn.execute(password_query)
+    password_query_obj = conn.execute(password_query)  # execute query that checks for valid password
     password_tuple = password_query_obj.fetchone()
     try:
-        password_result = password_tuple[0]
+        password_result = password_tuple[0] # if query returns a result, store it as a string
     except:
-        password_result = ""
+        password_result = ""  # if query returns no results/an error, set value as empty string
 
-    print("***If", email, "==", email_result, "and", password, "==", password_result)
     if email == email_result and password == password_result:
-        return True
+        return True  # if valid, login
     else:
-        return False
+        return False  # if invalid
 
-    conn.close()
+    conn.close()  # close database connection
