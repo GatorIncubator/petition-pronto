43a531267c747d631f9d8331823f58874bcd9614
clussier <lussierc@allegheny.edu>
2019-04-18 14:17:14 -0400

    Add Computer Science Department to Department table.

--- src/database_handler.py
+++ src/database_handler.py
@@ -1,6 +1,19 @@
+"""Gets petition info."""
+
+import sqlite3
+
 get_petitions(email):
     """Connects to database and gets list of petitions associated with the
     department of the input email."""
+    conn = sqlite3.connect("petitiondb.sqlite3")  # connect to the database
+    department_query = "SELECT department FROM User_Table WHERE email = \"{A}\"".format(A = email)
+    dept_query_obj = conn.execute(department_query)  # execute query that checks for valid email
+    dept_tuple = dept_query_obj.fetchone()  # store results of query - in a tuple
+    try:
+        email_result = email_tuple[0]  # if query returns a result, store it as a string
+    except:
+        email_result = ""  # if query returns no results/an error, set value as empty string
+    
 
 get_petition_info(id):
     """Connects to the database and gets info about a specific petition."""
