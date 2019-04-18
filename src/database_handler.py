c5aff7c927e71040dc5fb574273c0b5302fb02d7
clussier <lussierc@allegheny.edu>
2019-04-18 14:21:18 -0400

    Add recent work.

--- src/database_handler.py
+++ src/database_handler.py
@@ -6,14 +6,18 @@ get_petitions(email):
     """Connects to database and gets list of petitions associated with the
     department of the input email."""
     conn = sqlite3.connect("petitiondb.sqlite3")  # connect to the database
+
+    # Get department based on inputted email:
     department_query = "SELECT department FROM User_Table WHERE email = \"{A}\"".format(A = email)
-    dept_query_obj = conn.execute(department_query)  # execute query that checks for valid email
+    dept_query_obj = conn.execute(department_query)
     dept_tuple = dept_query_obj.fetchone()  # store results of query - in a tuple
     try:
-        email_result = email_tuple[0]  # if query returns a result, store it as a string
+        dept_result = email_tuple[0]  # if query returns a result, store it as a string
     except:
-        email_result = ""  # if query returns no results/an error, set value as empty string
-    
+        dept_result = ""  # if query returns no results/an error, set value as empty string
+
+    # Find petitions for that department
+
 
 get_petition_info(id):
     """Connects to the database and gets info about a specific petition."""
