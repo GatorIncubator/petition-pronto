1994a3c93a459460cf6fa1f4eb93225b7b7a365e
clussier <lussierc@allegheny.edu>
2019-04-18 14:07:46 -0400

    Create functions that will hold code for getting petition information.

---
+++ src/database_handler.py
@@ -0,0 +1,6 @@
+get_petitions(email):
+    """Connects to database and gets list of petitions associated with the
+    department of the input email."""
+
+get_petition_info(id):
+    """Connects to the database and gets info about a specific petition."""
