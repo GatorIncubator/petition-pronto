0c9aa0de3e5ca82c55a5976f2142b50f0e09e0f9
Jeremy Moore <jjmoore@ewire23-42.allegheny.edu>
2019-04-22 15:32:29 -0400

    Email messenger

---
+++ src/send_email.py
@@ -0,0 +1,23 @@
+import smtplib
+
+import config
+
+
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
+
+
+subject = "Petition-Pronto"
+msg = "Your Petition has been approved"
+
+send_email(subject, msg)
