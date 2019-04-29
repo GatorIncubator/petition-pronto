86132ccaa29aa3b13510358efea588538df0013c
clussier <lussierc@allegheny.edu>
2019-04-29 12:32:09 -0400

    Add comments for submit_decision().

--- src/database_handler.py
+++ src/database_handler.py
@@ -84,60 +84,67 @@ def create_account(email, password, role, department):
 
 
 def submit_decision(petitionID, approval_decision, email):
-    """Submits faculty's approval or denial decision for petition."""
+    """Checks if faculty has submitted a vote, submits a vote,
+    determines if petition is approved."""
     conn = sqlite3.connect("petitiondb.sqlite3")  # connect to the database
     cur = conn.cursor()
 
+    # find the faculty member's id:
     id_query = "SELECT id FROM User_Table WHERE email = \"{A}\"".format(A = email)
     id_query_obj = conn.execute(id_query)
     id_tuple = id_query_obj.fetchone()  # store results of query - in a tuple
     try:
-        id_result = id_tuple[0]  # if query returns a result, store it as a string
+        id_result = id_tuple[0]  # if query returns a result, store it in object
     except:
         id_result = ""  # if query returns no results/an error, set value as empty string
 
+    # find the given petition:
     find_petition_query = "SELECT * FROM Approval_Responses WHERE petitionID = {A}".format(A = petitionID)
     find_petition_query_obj = conn.execute(find_petition_query)
     petition_tuple = find_petition_query_obj.fetchall()
     try:
         approval_info = petition_tuple[0]
     except:
+        # if the petition does not exist in the approval table, add it:
         create_approval = "INSERT INTO Approval_Responses(petitionID, numOfResponses, numOfApprovals) VALUES({A}, {B}, {C})".format(A = petitionID, B = 0, C = 0)
         cur.execute(create_approval)  # execute the creation
         conn.commit()  # commit changes to the database
 
+    # get the number of responses and number of approvals for petition:
     num_query = "SELECT numOfResponses, numOfApprovals FROM Approval_Responses WHERE petitionID = {A}".format(A = petitionID)
     num_query_obj = conn.execute(num_query)
     num_tuple = num_query_obj.fetchall()
     try:
-        numOfResponses = num_tuple[0][0]
-        numOfApprovals = num_tuple[0][1]
+        numOfResponses = num_tuple[0][0]  # store the number of responses
+        numOfApprovals = num_tuple[0][1]  # store the number of approvals
     except:
         pass
 
+    # get the id's of faculty who have voted in the petition:
     get_petition_voters_query = "SELECT ID FROM Petition_Voters WHERE petitionID = {A}".format(A = petitionID)
     petition_voters_obj = conn.execute(get_petition_voters_query)
     petition_voters_list = petition_voters_obj.fetchall()
 
-    not_voted = False
-    if len(petition_voters_list) >= 1:
+    not_voted = False  # declare non_voted
+    if len(petition_voters_list) >= 1:  # check if there are any faculty voters for the petition
         for j in range(len(petition_voters_list)):
-            print(petition_voters_list[j][0])
+            # find if the faculty member has voted:
             if id_result == petition_voters_list[j][0]:
+                # the faculty member has voted
                 print("You have already voted.")
-                not_voted = False
             else:
                 print("You have not voted.")
-                not_voted = True
+                not_voted = True  # the faculty member has not yet voted
     else:
-        not_voted = True
+        not_voted = True  # if there are no voters, then the faculty member has not voted
 
-    if not_voted is True:
+    if not_voted is True:  # check if the faculty member has not voted
         print("Adding your vote.")
-        add_vote(approval_decision, petitionID, id_result, numOfResponses, numOfApprovals)
+        add_vote(approval_decision, petitionID, id_result, numOfResponses, numOfApprovals)  # submit new vote
     else:
         pass  # move on to counting votes
 
+    # get the student email for the petition:
     get_student_email_query = "SELECT email FROM Student_Petition WHERE petitionID = {A}".format(A = petitionID)
     get_student_email_obj = conn.execute(get_student_email_query)
     student_email_tuple = get_student_email_obj.fetchone()
@@ -146,6 +153,7 @@ def submit_decision(petitionID, approval_decision, email):
     except:
         student_email = ""
 
+    # get the department for the petition:
     get_petition_department = "SELECT department FROM Student_Petition WHERE petitionID = {A}".format(A = petitionID)
     get_department_obj = conn.execute(get_petition_department)
     department_tuple = get_department_obj.fetchone()
@@ -154,30 +162,33 @@ def submit_decision(petitionID, approval_decision, email):
     except:
         petition_department = ""
 
+    # get the faculty members of the petition department:
     get_faculty_emails_query = "SELECT email FROM User_Table WHERE department = {A}".format(A = petition_department)
     faculty_emails_obj = conn.execute(get_faculty_emails_query)
     faculty_email_list = faculty_emails_obj.fetchall()
 
-    if numOfResponses >= len(faculty_email_list):  # num of faculty
-        necessaryApprovals = math.ceil(len(faculty_email_list) / 2)
-        print(necessaryApprovals)
-        if numOfApprovals >= 2:
+    if numOfResponses >= len(faculty_email_list):  # check if all faculty for department have responded:
+        necessaryApprovals = math.ceil(len(faculty_email_list) / 2)  # calculate majority vote needed for approval
+        if numOfApprovals >= necessaryApprovals:  # check if student has the necessary amount of approvals
+            # set email messages for a passing result:
             student_message = "After votes by the department faculty, it was determined that your petition has been approved."
             teacher_message = "This email is to let you know that the reviewed petition by", student_email, "passed."
             print(teacher_message)
-        else:
+        else:  # student does not have necessary amount of approvals
+            # set email messages for a failing result:
             student_message = "After votes by department faculty, it was determined that your petition has been denied."
-            teacher_message = "This email is to let you know that the reviewed petition by", student_email, "did not pass."
-            print(teacher_message)
-        student_subject = "Information About Your Petition"
-        teacher_subject = "Information About Student Petition"
+            faculty_message = "This email is to let you know that the reviewed petition by", student_email, "did not pass."
+            print(faculty_message)
+
+        student_subject = "Information About Your Petition" # set subject of student email
+        faculty_subject = "Information About Student Petition" # set subject of faculty email
 
         send_email.send_email(student_subject, student_message, student_email)  # send email to the student
 
         for i in range(len(faculty_email_list)):
             current_email = faculty_email_list[i][0]
             print(current_email)
-            send_email.send_email(teacher_subject, teacher_message, current_email)  # send email to faculty member
+            send_email.send_email(faculty_subject, faculty_message, current_email)  # send email to faculty member
 
     conn.close()  # close database connection
 
